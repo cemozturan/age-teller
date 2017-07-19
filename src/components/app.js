b97bdd60d47b3f17ac3a0dff5508f8bc015dbbed
Cem Ozturan <cozturan@findmypast.com>
2017-07-19 14:34:11 +0100

    Implementing state, onChange and onClick events

--- src/components/app.js
+++ src/components/app.js
@@ -3,14 +3,29 @@ import { Form, FormControl, Button } from 'react-bootstrap';
 import './app.css';
 
 class App extends Component {
+  constructor() {
+    super(); // Required before we can use the "this" keyword
+    this.state = {
+      newDate: '',
+      birthday: '1999-06-21'
+    };
+  }
+
+  changeBirthday() {
+    this.setState({ birthday: this.state.newDate });
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="app">
         <Form inline>
         <h2>Input your birthday</h2>
-          <FormControl type="date" />
+          <FormControl
+            type="date"
+            onChange={ event => this.setState({newDate: event.target.value}) }/>
           {' '}
-          <Button>Submit</Button>
+          <Button onClick={() => this.changeBirthday()}>Submit</Button>
         </Form>
       </div>
     );
