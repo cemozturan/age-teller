7beda457ba751e5807358c31b6ef5ead96cc6e4c
Cem Ozturan <cozturan@findmypast.com>
2017-07-19 11:04:40 +0100

    Adding the input form and button (form submission not implemented yet)

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import './app.css';
 
 class App extends Component {
   render() {
     return (
-      <div>Age Teller Component</div>
+      <div className="app">
+        <Form inline>
+        <h2>Input your birthday</h2>
+          <FormControl type="date" />
+          {' '}
+          <Button>Submit</Button>
+        </Form>
+      </div>
     );
   }
 }
